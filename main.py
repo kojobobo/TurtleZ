ed50e58234fff35c64071cd669e8805b0cb5bc29
Joe <joekorc@gmail.com>
2021-02-19 12:26:57 -0500

    TurtleZ

---
+++ main.py
@@ -0,0 +1,61 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+from board import create_b
+
+
+MOVE_INCREMENT = 10
+screen = Screen()
+screen.colormode(255)
+screen.setup(width=1200, height=600)
+screen.tracer(0)
+screen.title("TurtleZ")
+screen.listen()
+create_b()
+turtle = Player()
+scoreboard = Scoreboard()
+scoreboard.score()
+screen.onkeyrelease(turtle.move_u, "Up")
+screen.onkeyrelease(turtle.move_d, "Down")
+game_is_on = True
+car = CarManager()
+cars = car.create_cars()
+
+
+while game_is_on:
+    time.sleep(0.1)
+    turtle_y = turtle.ycor()
+    game_over = car.move(MOVE_INCREMENT, turtle_y)
+
+    if game_over:
+        scoreboard.game_over()
+        replay = screen.textinput("You have Squished our turtle friend", "Enter 1 to restart, 2 to continue 3 to quit:")
+
+        if replay == "1":
+            car.done()
+            scoreboard.restart()
+            MOVE_INCREMENT = 10
+            car.create_cars()
+            turtle.restart()
+            screen.listen()
+
+        elif replay == "2":
+            turtle.restart()
+            scoreboard.game_on()
+            screen.listen()
+
+        else:
+            turtle.bye()
+
+    if turtle.ycor() > 250:
+        scoreboard.lx_score()
+        car.create_cars()
+        turtle.restart()
+        MOVE_INCREMENT += 3
+        time.sleep(0.01)
+
+    screen.update()
+
+screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,63 @@
+from turtle import Turtle
+
+
+
+FONT = ("Courier", 16, "normal")
+FONT2 = ("Courier", 100, "bold")
+FONT3 = ("Courier", 16, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.l_score = 0
+        with open("data.txt", 'r') as data:
+            data.seek(0)
+            self.high_score = int(data.read())
+
+        self.hideturtle()
+        self.penup()
+
+    def score(self):
+        self.color((222, 201, 136))
+        self.goto(-575, 250)
+        self.write(f"Level = {self.l_score}\nMax laps = {self.high_score}", align="left", font=FONT)
+        self.goto(275, 275)
+        self.write("Press Up to go forward", align="Left", font=FONT3)
+        self.goto(275, 250)
+        self.write("Press Down to go back", align="Left", font=FONT3)
+        self.goto(-25, 250)
+        self.write(f"Help our Turtle friend cross the road\nHe needs the excersize  ", align="Center", font=FONT3)
+
+    def lx_score(self):
+        self.l_score += 1
+        self.clear()
+        self.score()
+
+    def game_over(self):
+
+        if self.l_score > self.high_score:
+            self.high_score = self.l_score
+            with open("data.txt", 'w') as data:
+                data.seek(0)
+                data.write(str(self.high_score))
+            self.clear()
+            self.score()
+
+        self.goto(0, -50)
+        self.color(0, 0, 0)
+        self.write("GAME OVER", align="center", font=FONT2)
+        self.color((222, 201, 136))
+
+        if self.l_score > self.high_score:
+            self.high_score = self.l_score
+
+    def restart(self):
+        self.clear()
+        self.l_score = 0
+        self.score()
+
+    def game_on(self):
+        self.clear()
+        self.score()
